7997fc9f033832881e020a47932f9f29e8f1fc2c
Teppei Tosa <teppei_tosa@hotmail.com>
2014-03-18 00:15:37 +0900

    Complete prepare the 6th meeting

--- script.R
+++ script.R
@@ -3,8 +3,14 @@ setwd("~/dev/kaggle/ds2014")
 histories <- read.csv("train.csv")
 groups <- read.csv("groups.csv")
 
+summary(as.factor(groups$day))
+hist(groups$day)
+
 summary(groups$state)
 plot(groups$state)
+
+summary(groups$day)
+plot(groups$day)
 
 summary(as.factor(groups$location))
 plot(as.factor(groups$location))
@@ -35,7 +41,7 @@ summary(as.factor(groups$c_previous))
 summary(as.factor(groups$duration_previous))
 plot(as.factor(groups$duration_previous))
 
-summary(groups$isSingle_state1)
+summary(groups$isSingle_state)
 summary(groups$isSingle_location)
 summary(groups$isSingle_group_size)
 summary(groups$isSingle_homeowner)
@@ -48,9 +54,10 @@ summary(groups$isSingle_married_couple)
 summary(groups$isSingle_c_previous)
 summary(groups$isSingle_duration_previous)
 
-summary(groups$howPrevPolicy)
 summary(as.factor(groups$howPrevPolicy))
 hist(groups$howPrevPolicy)
+
+# 1以外、つまり直前見積もり採用以外は思った以上に偏りが少ない
 
 summary(groups$count)
 summary(as.factor(groups$count))
@@ -65,29 +72,53 @@ hist(groups$meanCost)
 summary(groups$maxCost)
 hist(groups$maxCost)
 
+summary(groups$daysCount)
+hist(groups$daysCount)
+
+summary(groups$howLong)
+hist(groups$howLong)
+
 # 直前見積もりを採用しなかった人
 notEve <- subset(groups, howPrevPolicy != 1)
 summary(as.factor(notEve$howPrevPolicy)) # 当然１がいないことを確認
 
 # 大体７割程度の人が直前見積もりを採用
 
-notEve$selectMin <- ifelse(notEve$finCost == notEve$minCost,1,0)
-summary(as.factor(notEve$selectMin))
-# 直前見積もりを採用しなかった人で、最小見積もりを採用した人は、7052/29016
+whichSelected <- ifelse(groups$finCost < groups$minCost, "underMin",
+                  ifelse(groups$finCost == groups$minCost, "eqMin",
+                  ifelse(groups$finCost == groups$maxCost, "eqMax",
+                  ifelse(groups$finCost > groups$maxCost, "overMax", "mid"))))
 
-notEve$selectMax <- ifelse(notEve$finCost == notEve$maxCost,1,0)
-summary(as.factor(notEve$selectMax))
-# 直前見積もりを採用しなかった人で、最大見積もりを採用した人は、8171/29016
+table(whichSelected)
 
-notEve$selectRatio <- (notEve$finCost - notEve$minCost) / (notEve$maxCost - notEve$minCost)
-summary(notEve$selectRatio)
-hist(notEve$selectRatio)
+# eqMax    eqMin      mid  overMax underMin 
+# 15741     9623    44339    15377    11929 
+#   16%      10%      46%      16%      12%
 
-# 直前見積もりを採用しなかった人で、
-# 最大見積もりを採用した人は、28%
-# 最小見積もりを採用した人は、24%
-# 中間値の見積もりを採用した人は、48%
-# 中間値のどの見積もりかは特徴が無さそう
+whichSelectedInNotEve <- ifelse(notEve$finCost < notEve$minCost, "underMin",
+                        ifelse(notEve$finCost == notEve$minCost, "eqMin",
+                        ifelse(notEve$finCost == notEve$maxCost, "eqMax",
+                        ifelse(notEve$finCost > notEve$maxCost, "overMax", "mid"))))
+
+table(whichSelectedInNotEve)
+
+# eqMax    eqMin      mid  overMax underMin 
+#   899      735    13812     7253     6317 
+#    3%     2.5%      48%      25%      22%
+
+# 直前見積もりを採用しなかった人は、
+# 最大見積もりか、最小見積もりかのどちらかを選択するかと
+# 思ったが、それは直前見積もりを採用しなかった人の5.5%しかいないので
+# あては外れた
+
+
+hist(notEve$howPrevPolicy)
+sum(ifelse(notEve$howPrevPolicy == notEve$count, 1, 0)) / 29016
+# 0.8628343
+
+# 直前見積もりを採用しなかった人は
+# 最初の見積もりを最終的に選択する可能性が極めて高いことが分かる
+
 
 groups2 <- groups
 
@@ -152,8 +183,8 @@ sum(groups2.valid.prob.01) / 29103
 # ひとまずこれで、直前見積もり採用組を予測するモデルは出来上がりとする
 
 
-groups3 <- groups # 最大見積もり選択モデル作成用データ
-groups3$selectMax <- ifelse(groups3$finCost == groups3$maxCost, 1, 0)
+groups3 <- groups # 最初の見積もり選択モデル作成用データ
+groups3$selectFirst <- ifelse(groups3$howPrevPolicy == groups3$count, 1, 0)
 
 # 効いてくるのが当たり前の変数を潰しておく
 groups3$finCost <- NULL
@@ -163,98 +194,57 @@ groups3.train <- groups3[trainnum, ]  # トレーニングデータ
 groups3.valid <- groups3[validnum, ]  # 検証データ
 
 
-groups3.tree = rpart(selectMax~., data=groups3.train, maxdepth=3, cp=-1)
+groups3.tree = rpart(selectFirst~., data=groups3.train, maxdepth=3, cp=-1)
 plot(as.party(groups3.tree))
 
 groups3.train.prob <- predict(groups3.tree, newdata=groups3.train)
 
 calcAR(X=groups3.train.prob, 
-       y=groups3.train$selectMax, 
+       y=groups3.train$selectFirst,
        TARGET="1", 
        plotCAP=TRUE, 
        plotpr=TRUE)
 
-# AR : 0.2656169
+# AR : 0.2778828
 
 groups3.valid.prob <- predict(groups3.tree, newdata=groups3.valid)
 
 calcAR(X=groups3.valid.prob, 
-       y=groups3.valid$selectMax, 
+       y=groups3.valid$selectFirst, 
        TARGET="1", 
        plotCAP=TRUE, 
        plotpr=TRUE)
 
-# AR : 0.2604701
+# AR : 0.2684538
 
 # オーバーフィッティングはしてなさそう。
 
-# これで最大見積もり選択モデルの作成は完了
-
-
-groups4 <- groups # 最大見積もり選択モデル作成用データ
-groups4$selectMin <- ifelse(groups4$finCost == groups4$minCost, 1, 0)
-
-# 効いてくるのが当たり前の変数を潰しておく
-groups4$finCost <- NULL
-groups4$howPrevPolicy <- NULL
-
-groups4.train <- groups4[trainnum, ]  # トレーニングデータ
-groups4.valid <- groups4[validnum, ]  # 検証データ
-
-groups4.tree = rpart(selectMin~., data=groups4.train, maxdepth=3, cp=-1)
-plot(as.party(groups4.tree))
-
-groups4.train.prob <- predict(groups4.tree, newdata=groups4.train)
-
-calcAR(X=groups4.train.prob, 
-       y=groups4.train$selectMin, 
-       TARGET="1", 
-       plotCAP=TRUE, 
-       plotpr=TRUE)
-
-# AR : 0.1963398
-
-groups4.valid.prob <- predict(groups4.tree, newdata=groups4.valid)
-
-calcAR(X=groups4.valid.prob, 
-       y=groups4.valid$selectMin, 
-       TARGET="1", 
-       plotCAP=TRUE, 
-       plotpr=TRUE)
-
-# AR : 0.1669494
-
-# オーバーフィッティングはしてなさそう。
-
-# これで最小見積もり選択モデルの作成は完了
+# これで最初の見積もり選択モデルの作成は完了
 
 
 test <- read.csv("groups.test.csv")
 
 test.eve.prob <- predict(groups2.tree, newdata=test)
-test.max.prob <- predict(groups3.tree, newdata=test)
-test.min.prob <- predict(groups4.tree, newdata=test)
+test.1st.prob <- predict(groups3.tree, newdata=test)
 
 testlen <- 55716
 
 test.prob <- rep(0, testlen)
 test.prob.flg <- rep(0, testlen)
 
+
 for(i in 1:testlen){
   if(test.eve.prob[i] > 0.58){
-    test.prob[i] <- test$evePolicy[i]
+    test.prob[i] <- as.character(test$evePolicy[i])
     test.prob.flg[i] <- "eve"
     
-  }else if(test.max.prob[i] > 0.3){
-    test.prob[i] <- test$maxCostPolicy[i]
-    test.prob.flg[i] <- "max"
-    
-  }else if(test.min.prob[i] > 0.2){
-    test.prob[i] <- test$minCostPolicy[i]
-    test.prob.flg[i] <- "min"
+  }else if(test.1st.prob[i] > 0.5){
+    test.prob[i] <- as.character(test$firstPolicy[i])
+    test.prob.flg[i] <- "1st"
     
   }else{
-    test.prob[i] <- test$evePolicy[i]
+    # どちらにもあてはまらない人はえいやで直前見積もりにしてしまう
+    test.prob[i] <- as.character(test$evePolicy[i])
     test.prob.flg[i] <- "eve"
     
   }
@@ -262,9 +252,11 @@ for(i in 1:testlen){
 
 table(test.prob.flg)
 
-answer <- data.frame(customer_ID=test$customer_ID, plan=test.prob)
+answer <- data.frame(customer_ID=test$customer_ID, plan=substr(test.prob, 2, 8))
 
 write.csv(answer, "answer.csv", row.names=F)
-
+# 最後に
+# sed -e 's/\"//g' answer.csv > answer2.csv
+# をして、ダブルクオートを削除して提出
 
 
